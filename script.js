31171e952ba0b62c6e11722f198540f983c1dcde
Ouissal EL HAMAMI <ouissal.elhamami@gmail.com>
2023-06-03 19:59:22 +0200

    rainbob and eraser functionalities with a new design

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
 
+    let currentMode='color';
+    let currenColor;
+
+    let colorPicker = document.getElementById('colorPicker');
+    currenColor=colorPicker.value;
+    colorPicker.addEventListener('change',function(){
+        currenColor=colorPicker.value;  
+    })
     let divEle = document.createElement('div');
+    let pen = true;
     divEle.className = 'grid';
     let container = document.querySelector('.container');
     let Cwidth = container.offsetWidth;
     let Cheight = container.offsetHeight;
     
     let clearBtn=document.querySelector('#clear');
-    
+    let eraserBtn=document.querySelector('#eraser');
+    let painBtn = document.querySelector('#paintBtn');
+    let rainbowBtn=document.querySelector('#rainbow');
+    let darkerBtn= document.querySelector('#darker');
+    let lighterBtn= document.querySelector('#lighter');
+
     let btn = document.querySelector('button');
     let input;
     let slider = document.getElementById('mySlider');
     let label=document.querySelector('label');
-
+console.log(slider.value);
     let isMouseDown = false;
     let isMouseup=true;
     let computedstyle=getComputedStyle(container);
     let bgColor=computedstyle.backgroundColor;
-/*container.addEventListener('mousedown', function() {
-  isMouseDown = true;
-  isMouseup=false;
-  console.log('down');
-});
-container.addEventListener('mouseup', function() {
-    isMouseDown = false;
-    isMouseup=true;
-    console.log('up');
-  });*/
+
    
   function draw(){
+    
     let grids = document.querySelectorAll('.grid');
-            
+    currenColor=colorPicker.value;
             
     grids.forEach(function(grid) {
       grid.addEventListener('mousedown', function() {
         isMouseDown = true;
-        grid.style.backgroundColor = 'black';
+        grid.style.backgroundColor = currenColor;
     })
         
         grid.addEventListener('mouseup', function() {
@@ -44,47 +50,41 @@ container.addEventListener('mouseup', function() {
         grid.addEventListener('mouseenter', function() {
            
           if (isMouseDown) {
-            grid.style.backgroundColor = 'black';
+            grid.style.backgroundColor = currenColor;
           }
         });
       });
+      currentMode='color'
   }
 
 
 
     function paint(){
+        pen=true;
         input = parseInt(slider.value);
-         
         label.textContent= input + ' x ' + input;
 
-
         while (container.firstChild) {
-            container.removeChild(container.firstChild);
-          }
-        
-        
-         
+            container.removeChild(container.firstChild);}
+                 
          let x=input*input;
          Cwidth = container.offsetWidth;
          Cheight = container.offsetHeight;
          
          divEle.style.width= Cwidth/input +'px';
-            divEle.style.height= Cheight/input+'px';
+         divEle.style.height= Cheight/input+'px';
            
          for(let i=0; i<x;i++){
-          
-           
             let clonedChild = divEle.cloneNode(true);
-            
             container.appendChild(clonedChild);
-        
-            
-          
             }
-            
-            
-              
+               if(currentMode==='color')           
             draw();
+            else if (currentMode==='rainbow'){
+                rainbow();}
+                else if(currentMode==='eraser'){
+                    erase();
+                }
            
                            
     }
@@ -99,7 +99,119 @@ container.addEventListener('mouseup', function() {
         grid.style.backgroundColor = bgColor;
       });
     }
+
+function erase(){
+    pen=false;
+    let grids = document.querySelectorAll('.grid');
+            
+            
+    grids.forEach(function(grid) {
+      grid.addEventListener('mousedown', function() {
+        isMouseDown = true;
+        grid.style.backgroundColor = bgColor;
+    })
+        
+        grid.addEventListener('mouseup', function() {
+            isMouseDown = false;
+          });
+        
+        grid.addEventListener('mouseenter', function() {
+           
+          if (isMouseDown) {
+            grid.style.backgroundColor = bgColor;
+          }
+        });
+    
+      });
+      currentMode='eraser';
+}
+function setCurrenMode(newMode){
+
+    currentMode=newMode
+    if (currentMode==='color'){
+        painBtn.classList.add('active');
+       if(eraserBtn.classList.contains('active')) eraserBtn.classList.remove('active');
+       if(rainbowBtn.classList.contains('active')) rainbowBtn.classList.remove('active');
+       if(darkerBtn.classList.contains('active')) darkerBtn.classList.remove('active');
+       if(lighterBtn.classList.contains('active')) lighterBtn.classList.remove('active');
+    }
+    if (currentMode==='erase'){
+        eraserBtn.classList.add('active');
+       if(painBtn.classList.contains('active')) painBtn.classList.remove('active');
+       if(rainbowBtn.classList.contains('active')) rainbowBtn.classList.remove('active');
+       if(darkerBtn.classList.contains('active')) darkerBtn.classList.remove('active');
+       if(lighterBtn.classList.contains('active')) lighterBtn.classList.remove('active');
+    }
+    if (currentMode==='rainbow'){
+        rainbowBtn.classList.add('active');
+       if(painBtn.classList.contains('active')) painBtn.classList.remove('active');
+       if(eraserBtn.classList.contains('active')) eraserBtn.classList.remove('active');
+       if(darkerBtn.classList.contains('active')) darkerBtn.classList.remove('active');
+       if(lighterBtn.classList.contains('active')) lighterBtn.classList.remove('active');
+    }
+    if (currentMode==='darker'){
+        darkerBtn.classList.add('active');
+       if(painBtn.classList.contains('active')) painBtn.classList.remove('active');
+       if(eraserBtn.classList.contains('active')) eraserBtn.classList.remove('active');
+       if(rainbowBtn.classList.contains('active')) rainbowBtn.classList.remove('active');
+       if(lighterBtn.classList.contains('active')) lighterBtn.classList.remove('active');
+    }
+    if (currentMode==='lighter'){
+        lighterBtn.classList.add('active');
+       if(painBtn.classList.contains('active')) painBtn.classList.remove('active');
+       if(eraserBtn.classList.contains('active')) eraserBtn.classList.remove('active');
+       if(rainbowBtn.classList.contains('active')) rainbowBtn.classList.remove('active');
+       if(darkerBtn.classList.contains('active')) darkerBtn.classList.remove('active');
+    }
+
+}
+function getRandomRgbColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
+  }
+function rainbow(){
+    let grids = document.querySelectorAll('.grid');
+    
+            
+    grids.forEach(function(grid) {
+      grid.addEventListener('mousedown', function() {
+        isMouseDown = true;
+        grid.style.backgroundColor = getRandomRgbColor();
+    })
+        
+        grid.addEventListener('mouseup', function() {
+            isMouseDown = false;
+          });
+        
+        grid.addEventListener('mouseenter', function() {
+           
+          if (isMouseDown) {
+            grid.style.backgroundColor = getRandomRgbColor();
+          }
+        });
+      });
+      currentMode='rainbow';
+
+}
+function darker(){
+    
+}
+
+    
+    painBtn.onclick = ()=> setCurrenMode('color');
+    eraserBtn.onclick = ()=> setCurrenMode('erase');
+    rainbowBtn.onclick = ()=> setCurrenMode('rainbow');
+    darkerBtn.onclick = ()=> setCurrenMode('darker');
+    lighterBtn.onclick = ()=> setCurrenMode('lighter');
+    
+    eraserBtn.addEventListener('click',erase);
     clearBtn.addEventListener('click',clear);
+    painBtn.addEventListener('click',paint)
+    rainbowBtn.addEventListener('click',rainbow)
+
+
 
     paint();
    
