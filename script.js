c1f4ef52a1ebf9d2ca5859a0725c93ccc6a47c54
Ouissal EL HAMAMI <ouissal.elhamami@gmail.com>
2023-06-04 22:38:52 +0200

    adding some organisation

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 
     let currentMode='color';
-    let currenColor;
-
     let colorPicker = document.getElementById('colorPicker');
-    currenColor=colorPicker.value;
+    
+    let currenColor=colorPicker.value;
+
+    console.log(currenColor) ;
     colorPicker.addEventListener('change',function(){
-        currenColor=colorPicker.value;  
+        currenColor=colorPicker.value; 
+        
+        paint()
     })
+
     let divEle = document.createElement('div');
-    let pen = true;
     divEle.className = 'grid';
+
     let container = document.querySelector('.container');
     let Cwidth = container.offsetWidth;
     let Cheight = container.offsetHeight;
-    
     let clearBtn=document.querySelector('#clear');
     let eraserBtn=document.querySelector('#eraser');
     let painBtn = document.querySelector('#paintBtn');
@@ -21,157 +24,79 @@
     let darkerBtn= document.querySelector('#darker');
     let lighterBtn= document.querySelector('#lighter');
 
-    let btn = document.querySelector('button');
-    let input;
     let slider = document.getElementById('mySlider');
     let label=document.querySelector('label');
-console.log(slider.value);
-    let isMouseDown = false;
-    let isMouseup=true;
     let computedstyle=getComputedStyle(container);
     let bgColor=computedstyle.backgroundColor;
     let shader;
-   
-  function draw(){
     
-    let grids = document.querySelectorAll('.grid');
-    currenColor=colorPicker.value;
-            
-    grids.forEach(function(grid) {
-      grid.addEventListener('mousedown', function() {
-        isMouseDown = true;
-        grid.style.backgroundColor = currenColor;
-    })
-        
-        grid.addEventListener('mouseup', function() {
-            isMouseDown = false;
-          });
-        
-        grid.addEventListener('mouseenter', function() {
-           
-          if (isMouseDown) {
-            grid.style.backgroundColor = currenColor;
-          }
-        });
+   
+    let pen = true;
+    let isMouseDown = false;
+    let isMouseup=true;
+
+function fill(color){
+  
+  let grids = document.querySelectorAll('.grid')
+  grids.forEach(function(grid) {
+    grid.addEventListener('mousedown', function() {
+      isMouseDown = true;
+      grid.style.backgroundColor = color;
+  })
+    
+    grid.addEventListener('mouseup', function() {
+        isMouseDown = false;
       });
-      currentMode='color'
-  }
-
-
-
-    function paint(){
-        pen=true;
-        input = parseInt(slider.value);
-        label.textContent= input + ' x ' + input;
-
-        while (container.firstChild) {
-            container.removeChild(container.firstChild);}
-                 
-         let x=input*input;
-         Cwidth = container.offsetWidth;
-         Cheight = container.offsetHeight;
-         
-         divEle.style.width= Cwidth/input +'px';
-         divEle.style.height= Cheight/input+'px';
-         
-           
-         for(let i=0; i<x;i++){
-            let clonedChild = divEle.cloneNode(true);
-          
-            container.appendChild(clonedChild);
-            }
-               if(currentMode==='color')           
-            draw();
-            else if (currentMode==='rainbow'){
-                rainbow();}
-                else if(currentMode==='eraser'){
-                    erase();
-                }
-                else if(currentMode==='darker'){
-                    darker();
-                } else if(currentMode==='darker'){
-                    lighter();
-                }
-           
-                           
-    }
-
-    function clear(){
-
-        let grids = document.querySelectorAll('.grid');
-            
-            
-    grids.forEach(function(grid) {
-      
-        grid.style.backgroundColor = bgColor;
+    
+    grid.addEventListener('mouseenter', function() {
+        if (isMouseDown) grid.style.backgroundColor = color;
       });
-    }
+  });
+}
+// filling the grids with correspendant color
+function draw(){
+ currenColor=colorPicker.value;
+  fill(currenColor);
+    currentMode='color';
+}
+
+
+// creating grids and painting depends on currentMode
+
+
+function clear(){
+  let grids = document.querySelectorAll('.grid'); 
+  grids.forEach(function(grid) {
+    console.log(getComputedStyle(grid).backgroundColor)
+    grid.style.backgroundColor = bgColor;
+    console.log(getComputedStyle(grid).backgroundColor)
+  });
+}
 
 function erase(){
-    pen=false;
-    let grids = document.querySelectorAll('.grid');
-            
-            
-    grids.forEach(function(grid) {
-      grid.addEventListener('mousedown', function() {
-        isMouseDown = true;
-        grid.style.backgroundColor = bgColor;
-    })
-        
-        grid.addEventListener('mouseup', function() {
-            isMouseDown = false;
-          });
-        
-        grid.addEventListener('mouseenter', function() {
-           
-          if (isMouseDown) {
-            grid.style.backgroundColor = bgColor;
-          }
-        });
-    
-      });
-      currentMode='eraser';
+  pen=false;
+  fill(bgColor);
+    currentMode='eraser';
 }
-function setCurrenMode(newMode){
 
-    currentMode=newMode
-    if (currentMode==='color'){
-        painBtn.classList.add('active');
-       if(eraserBtn.classList.contains('active')) eraserBtn.classList.remove('active');
-       if(rainbowBtn.classList.contains('active')) rainbowBtn.classList.remove('active');
-       if(darkerBtn.classList.contains('active')) darkerBtn.classList.remove('active');
-       if(lighterBtn.classList.contains('active')) lighterBtn.classList.remove('active');
-    }
-    if (currentMode==='erase'){
-        eraserBtn.classList.add('active');
-       if(painBtn.classList.contains('active')) painBtn.classList.remove('active');
-       if(rainbowBtn.classList.contains('active')) rainbowBtn.classList.remove('active');
-       if(darkerBtn.classList.contains('active')) darkerBtn.classList.remove('active');
-       if(lighterBtn.classList.contains('active')) lighterBtn.classList.remove('active');
-    }
-    if (currentMode==='rainbow'){
-        rainbowBtn.classList.add('active');
-       if(painBtn.classList.contains('active')) painBtn.classList.remove('active');
-       if(eraserBtn.classList.contains('active')) eraserBtn.classList.remove('active');
-       if(darkerBtn.classList.contains('active')) darkerBtn.classList.remove('active');
-       if(lighterBtn.classList.contains('active')) lighterBtn.classList.remove('active');
-    }
-    if (currentMode==='darker'){
-        darkerBtn.classList.add('active');
-       if(painBtn.classList.contains('active')) painBtn.classList.remove('active');
-       if(eraserBtn.classList.contains('active')) eraserBtn.classList.remove('active');
-       if(rainbowBtn.classList.contains('active')) rainbowBtn.classList.remove('active');
-       if(lighterBtn.classList.contains('active')) lighterBtn.classList.remove('active');
-    }
-    if (currentMode==='lighter'){
-        lighterBtn.classList.add('active');
-       if(painBtn.classList.contains('active')) painBtn.classList.remove('active');
-       if(eraserBtn.classList.contains('active')) eraserBtn.classList.remove('active');
-       if(rainbowBtn.classList.contains('active')) rainbowBtn.classList.remove('active');
-       if(darkerBtn.classList.contains('active')) darkerBtn.classList.remove('active');
-    }
+function setCurrenMode(newMode) {
+  currentMode=newMode;
+  const modes = ['color', 'erase', 'rainbow', 'darker', 'lighter'];
+  const buttons = [painBtn, eraserBtn, rainbowBtn, darkerBtn, lighterBtn];
 
+  for (let i = 0; i < modes.length; i++) {
+    const mode = modes[i];
+    const button = buttons[i];
+
+    if (currentMode === mode) {
+      button.classList.add('active');
+    } else {
+      button.classList.remove('active');
+    }
+  }
 }
+
+
 function getRandomRgbColor() {
     const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
@@ -203,13 +128,12 @@ function rainbow(){
       currentMode='rainbow';
 
 }
+
 let rgbRegex = /rgb\((\d+),\s*(\d+),\s*(\d+)\)/;
 let updatedRed;
 let updatedGreen ;
 let updatedBlue ;
-      
 let updatedColor;
-
 let incrementRed;
 let incrementGreen ;
 let incrementBlue ;
@@ -220,7 +144,7 @@ function darker() {
         let c = getComputedStyle(grid).backgroundColor;
         if(c==='rgba(0, 0, 0, 0)'){
             c=bgColor;
-            console.log('nulllllll')
+            console.log('nulllllll');
         }
       grid.addEventListener('mousedown', function() {
         isMouseDown = true;
@@ -270,6 +194,7 @@ function lighter(){
    
     grids.forEach(function(grid) {
         let c = getComputedStyle(grid).backgroundColor;
+       
         if(c==='rgba(0, 0, 0, 0)'){
             c=bgColor;
             console.log('nulllllll')
@@ -330,6 +255,41 @@ function lighter(){
     darkerBtn.addEventListener('click',darker);
     lighterBtn.addEventListener('click',lighter);
 
+
+    function paint(){
+      currenColor=colorPicker.value
+
+      pen=true;
+      let input = parseInt(slider.value);
+      label.textContent= input + ' x ' + input;
+      while (container.firstChild) {  container.removeChild(container.firstChild); }
+                
+      let x=input*input;
+      Cwidth = container.offsetWidth;
+      Cheight = container.offsetHeight;
+      divEle.style.width= Cwidth/input +'px';
+      divEle.style.height= Cheight/input+'px';
+      
+      for(let i=0; i<x;i++){
+        let clonedChild = divEle.cloneNode(true);
+        container.appendChild(clonedChild);
+      }
+              if(currentMode==='color') {  
+                       
+                  draw();}
+          else if (currentMode==='rainbow'){
+              rainbow();}
+              else if(currentMode==='eraser'){
+                  erase();
+              }
+              else if(currentMode==='darker'){
+                  darker();
+              } else if(currentMode==='lighter'){
+                  lighter();
+              }
+            
+                            
+    }
     paint();
    
     slider.addEventListener('input',paint);
